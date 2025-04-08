8a60c0098aa08680e07a332ddacf59c46e33c9ea
techtee19 <techtee19@gmail.com>
2025-04-08 10:40:10 +0100

    more async functions

--- script.js
+++ script.js
@@ -494,18 +494,50 @@ const whereAmI = async function () {
     }
     console.log('Finished getting location');
   });
-  */
-
-const get3Countries = async function (c1, c2, c3) {
+  
+  const get3Countries = async function (c1, c2, c3) {
   try {
     const data = await Promise.all([
       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
       getJSON(`https://restcountries.com/v3.1/name/${c3}`),
     ]);
-
-    console.log(data);
+    
+    console.log(data.map(d => d[0].capital));
   } catch (err) {
     console.error(err);
   }
 };
+
+get3Countries('portugal', 'canada', 'nigeria');
+*/
+
+//Promise.race
+(async function () {
+  const res = Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/egypt`),
+    getJSON(`https://restcountries.com/v3.1/name/mexico`),
+  ]);
+  console.log(res[0]);
+});
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(() => {
+      reject(new Error('Request took too long!'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([getJSON(`https://restcountries.com/v3.1/name/italy`), timeout(1)])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
